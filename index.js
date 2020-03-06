35e4422d5f3ab2849fa87e4179d060dac363c09b
charliegabriella <charlotkroon@gmail.com>
2020-03-06 10:36:41 +0100

    Added express

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+const port = 4000;
+
+const db = {};
+
+db.messages = [];
+
+const parser = express.json();
+app.use(parser);
+
+app.post("/message", (request, response) => {
+  const { text } = request.body;
+
+  db.messages.push(text);
+
+  response.send(text);
+
+  console.log("db text:", db);
+});
+
+app.listen(port, () => console.log(`Listening on :${port}`));
